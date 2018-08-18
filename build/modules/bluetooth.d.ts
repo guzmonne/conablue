909a5c4fe6664aedaa00352810cee95bb1641eb9
Guzman Monne <guzmonne@hotmail.com>
2018-08-18 01:24:39 -0300

    Updates

--- build/modules/bluetooth.d.ts
+++ build/modules/bluetooth.d.ts
@@ -1,9 +1,22 @@
 /// <reference types="web-bluetooth" />
+export interface IBluetoothCharacteristic {
+    number: number;
+    label: string;
+}
+export interface IBluetoothCharacteristicsMap {
+    [key: string]: IBluetoothCharacteristic[];
+}
+interface RequestDeviceOptions {
+    filters: any[];
+}
 export declare class Bluetooth {
     device: BluetoothDevice;
     server: BluetoothRemoteGATTServer;
+    characteristics: IBluetoothCharacteristicsMap;
     connect(options?: RequestDeviceOptions): Promise<void>;
     defaultInfo(): Promise<string>;
     batteryLevel(): Promise<string>;
+    userData(): Promise<string>;
     deviceInformation(): Promise<string>;
 }
+export {};
